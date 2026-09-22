7f4fcdbbe01ab64ae7c08e9a13640608cc4be7f0
NikhilRW <nikhilwankhede0707@gmail.com>
2026-09-22 12:47:11 +0530

    native-sim: 2026-09-22T07:17:10.571Z

--- App.tsx
+++ App.tsx
@@ -1,13 +1,38 @@
-import { View } from 'react-native'
-import React from 'react'
-import MapView from 'react-native-maps'
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import MapView, {Marker, PROVIDER_DEFAULT, PROVIDER_GOOGLE} from 'react-native-maps';
 
 const App = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <MapView />
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_DEFAULT}
+        style={StyleSheet.absoluteFill}
+        initialRegion={{
+          latitude: 28.481,
+          longitude: 77.079,
+          latitudeDelta: 0.08,
+          longitudeDelta: 0.08,
+        }}>
+        <Marker
+          coordinate={{latitude: 28.495, longitude: 77.088}}
+          title="DLF Cyber Hub"
+          description="Gurgaon, Haryana"
+        />
+        <Marker
+          coordinate={{latitude: 28.467, longitude: 77.069}}
+          title="Kingdom of Dreams"
+          description="Gurgaon, Haryana"
+        />
+      </MapView>
     </View>
-  )
-}
+  );
+};
 
-export default App
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
